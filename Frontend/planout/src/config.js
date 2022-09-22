bc3748153bcdae85a49851b363d7d1b8611b2a7d
cococokoko <coco.koban@student.ie.edu>
2022-09-22 12:21:53 +0200

    backend link

--- Frontend/planout/src/config.js
+++ Frontend/planout/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-apiUrl: process.env.REACT_APP_API_URL,
+apiUrl: process.env.REACT_APP_API_URL || 'https://github-rllunraola-uc.a.run.app',
 apiMaps: process.env.REACT_APP_GOOGLE_MAP_API_KEY,
 };
 
